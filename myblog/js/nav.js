177759a0fa992e1d593a95bf329758eeb5b0f4a1
Eight <380080496@qq.com>
2018-01-23 12:50:50 +0800

    Update nav.js

--- myblog/js/nav.js
+++ myblog/js/nav.js
@@ -2,37 +2,50 @@ new Vue({
   el: '#blog',
   data: {
     news : [
+           	{
+            name : '首页',
+            new_tab : '../home/index.html',
+            fa : 'fa fa-home'
+        },
         {
             name : '最新新闻',
             new_tab : '../new/new.html',
+            fa : 'fa fa-podcast'
         },
         {
             name : '技术宅',
             new_tab : '../technology/technology.html',
+            fa : 'fa fa-universal-access'
         },
         {
             name : 'WEB前端',
             new_tab : '../web/web.html',
+            fa : 'fa fa fa-desktop'
         },
         {
             name : '后端',
             new_tab : '../backend/back.html',
+            fa :'fa fa-database'
         },
         {
             name : '服务器',
             new_tab : '../server/server.html',
+            fa : 'fa fa-cloud-upload'
         },
         {
             name : '科技新闻',
             new_tab : '../science/science.html',
+            fa : 'fa fa-commenting-o'
         },
         {
             name : '书山有路',
             new_tab : '../book/book.html',
+            fa : 'fa fa fa-book'
         },
         {
             name : '其他',
             new_tab : '../other/other.html',
+            fa : 'fa fa-user-secret'
         }
     ],
     hade_img : '../../imgs/psu.jpg',
